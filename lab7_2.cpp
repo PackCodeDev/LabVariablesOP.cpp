8f9ae9b105612653be355a1e307e20f99c61934a
PackCodeDev <sirikorn_wongsa@cmu.ac.th>
2025-12-15 17:19:07 +0700

    Lap7_123.cpp

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,5 +1,52 @@
 //Dialog of the first example is given below.
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main(){
+    string name ;
+    string stuid;
+    string movie;
+    string day;
+    string massges;
+    int gear, year;
+
+
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?"<< endl;
+    cout << "?????: ";
+    getline(cin,name);
+
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name." << endl;
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << endl;
+    cout << name << ": " ;
+    cin >> stuid;
+    cin.ignore();
+
+    string firstTwoDigits = stuid.substr(0,2);
+
+    year = stoi (firstTwoDigits);
+    gear = year - 12;
+
+
+
+    cout <<"Fahsai: I think you may be GEAR " << gear    <<". I have a free movie ticket for you."<< endl;
+    cout << "Fahsai: Let's go to the cinema together!!!" << endl;
+    cout << "Fahsai: What movie do you want to watch?" << endl;
+
+    cout << name << ": ";
+    getline(cin,movie);
+    cout << "Fahsai: So....which day are you free to go with me?" << endl;
+
+    cout << name << ": "; ;
+    getline(cin , day);
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you."<< endl;
+    cout << name <<": " ;
+    getline (cin , massges);
+    cout << "Fahsai: 555+ see you " << day <<". Bye Bye \\\(^ ^)/";
+    return 0;
+
+
+}
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
 ?????: Luke Skywalker
